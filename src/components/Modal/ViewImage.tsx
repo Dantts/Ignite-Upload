917878abb34aa308764d91acebff3b8be3709d9f
Dantts <gabrielteixeiradantas@hotmail.com>
2022-06-24 20:50:31 -0300

    feat: desafio finalizado

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -18,6 +18,21 @@ export function ModalViewImage({
   isOpen,
   onClose,
   imgUrl,
-}: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+}: ModalViewImageProps) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent alignSelf="center" bgColor="pGray.800">
+        <ModalBody p={2}>
+          <Image src={imgUrl} />
+        </ModalBody>
+
+        <ModalFooter justifyContent="start">
+          <Link href={imgUrl} target="_blank">
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
